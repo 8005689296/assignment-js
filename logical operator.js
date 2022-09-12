d9fb63810197404f5f07927efb572fd3f231b33d
Ritik <ritikkhandelwal650@gmail.com>
2022-09-12 05:06:48 -0700

    check interger

---
+++ logical operator.js
@@ -0,0 +1,10 @@
+//Write a JavaScript program that accept two integers and display the larger.
+
+const a =12;
+const b = 23;
+if(a>b){
+            console.log("largest interger is A");
+
+}else {
+            console.log("largest interger is b");
+}

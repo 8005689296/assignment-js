76f2d6837e629923cbb73635c966f6978312f07e
Ritik <ritikkhandelwal650@gmail.com>
2022-09-12 05:11:49 -0700

    even and odd

---
+++ even and odd.js
@@ -0,0 +1,10 @@
+//Even or OddMedium
+//Write a JavaScript program that checks if input number is even or odd by using “%” operator in JavaScript.
+
+var num3 = 17.5; 
+var num4 = 42;
+if(num3%2==0 || num4%2==0){
+            console.log("num3 and num4 is even number");
+}else{
+            console.log("odd");
+}

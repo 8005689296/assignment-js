ac11f289f7a8922db016a057357fc07678484ab7
Ritik <ritikkhandelwal650@gmail.com>
2022-09-12 04:19:33 -0700

    uppercase

---
+++ array map.js
@@ -0,0 +1,13 @@
+//Using Map() methodMedium
+//Use the .map() method on the heros array to return a new array.
+
+//The new array should rename the 'name' key to 'hero'.
+//The 'name' key should not appear in the new array.
+//The new array should have a new key added called (id).
+//The key 'id' should be based on the index.
+
+const heros = [
+ { name: 'Spider-Man' }, { name: 'Thor' }, { name: 'Black Panther' },
+ { name: 'Captain Marvel' }, { name: 'Silver Surfer' } ];
+            
+console.log(heros.name);
---
+++ uppercase.js
@@ -0,0 +1,6 @@
+//Uppercase ArrayEasy
+//Create a new array whose elements is in uppercase of words present in the original array.
+
+const arr =  ["avengers", "captain america", "ironman", "black panther"];
+const re = arr.map(arr  =>arr.toUpperCase());
+console.log(re);

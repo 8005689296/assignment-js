cc5f0b59796743468e72ac9b1065aef85cc9626f
Ritik <ritikkhandelwal650@gmail.com>
2022-09-12 04:07:00 -0700

    join

---
+++ arrayassignment.js
@@ -0,0 +1,7 @@
+//Joining elements in ArrayEasy
+//Write a simple JavaScript program to join all elements of the following array into a string,
+// once simply and once with the '+' sign in between the elements.
+
+const arr = ["Red", "Green", "White", "Black"];
+const result = arr.join("+");
+console.log(result);

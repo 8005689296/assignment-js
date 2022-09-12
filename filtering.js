2e2ba805927181f181f54b578933ea1ad4162a4e
Ritik <ritikkhandelwal650@gmail.com>
2022-09-12 05:02:47 -0700

    filtering

---
+++ filtering.js
@@ -0,0 +1,7 @@
+//Filtering Array
+//Write JavaScript statements that will produce the output mentioned in output 
+//format from array in input formamat
+
+const inputWords = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+const result = inputWords.slice(-3);
+console.log(result);

aa231ad53e558670d696d8b3e2dd90c5a77085e8
Ritik <ritikkhandelwal650@gmail.com>
2022-09-11 22:23:09 -0700

    opeartor

---
+++ string and operator.js
@@ -0,0 +1,18 @@
+//javascript Operators Assign two numeric variables a and b Initialise values as 5 and 10
+// respectively and console log in same order the following:
+
+//Create a variable add and assign it the sum of a and b.
+//Create a variable sub and assign it result of b subtracted from a.
+//Create a variable mul and assign it the product of a and b.
+//Create a variable div and assign it the result of of a divided by b.
+//Create a variable inc and assign it the preincremented value of a.
+//Create a variable dec and assign it the predecremented value of b.
+
+const a = 12;
+const b = 13;
+console.log(a+b);// adding
+console.log(a-b);//subtract
+console.log(a*b);//multiply
+console.log(a/b);//divide
+console.log (++a);//preincement
+console.log(--b);//predecremented
